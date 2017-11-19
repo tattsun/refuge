3c0a4fcb32301aaa534e20b5ed12871b317f87cf
tattsun <t.t.mc1192.sf@gmail.com>
2017-11-19 21:57:54 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net"
+	"encoding/base64"
+	"io"
+	"sync"
+	"github.com/pkg/errors"
+)
+
+
+func handleProxy(w http.ResponseWriter, r *http.Request) {
+	hj, _ := w.(http.Hijacker)
+
+	proxy, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer proxy.Close()
+
+	client, _, err := hj.Hijack()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	auth := "test:pass"
+	authb64 := base64.StdEncoding.EncodeToString([]byte(auth))
+	r.Header.Set("Proxy-Authorization", "Basic " + authb64)
+	if err = r.Write(proxy); err != nil {
+		log.Fatal(errors.Wrap(err, "failed to write request "))
+	}
+
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
+	go func() {
+		io.Copy(client, proxy)
+		wg.Done()
+	}()
+	go func() {
+		io.Copy(proxy, client)
+		wg.Done()
+	}()
+	wg.Wait()
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":86", http.HandlerFunc(handleProxy)))
+}
